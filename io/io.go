263a9798bbbc575b1b6dfaa3b742db59d4512f3f
punkycommunist <punkycommunist@protonmail.com>
2020-11-26 13:22:08 +0100

    updated Version and .gitignore

--- io/io.go
+++ io/io.go
@@ -19,7 +19,7 @@ import (
 )
 
 //Version is the version of the compiled source
-const Version string = "1.1.3"
+const Version string = "2.0.0"
 
 const defaultSettings string = `{
 	"QDayAverage": {
