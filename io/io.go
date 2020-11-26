a0010fc57ae8bce8065d0ccac18398da2793018c
punkycommunist <punkycommunist@protonmail.com>
2020-11-26 14:09:49 +0100

    changed version

--- io/io.go
+++ io/io.go
@@ -19,7 +19,7 @@ import (
 )
 
 //Version is the version of the compiled source
-const Version string = "2.0.0"
+const Version string = "2.0.1"
 
 const defaultSettings string = `{
 	"QDayAverage": {
